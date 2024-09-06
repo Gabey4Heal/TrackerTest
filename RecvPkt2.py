86c278b010e58d1e1d1737d34dda77477042d84e
Gabey <130712384+GabeyDev@users.noreply.github.com>
2024-09-06 14:12:37 -0300

    Update RecvPkt2.py

--- RecvPkt2.py
+++ RecvPkt2.py
@@ -73,6 +73,21 @@ def parse_data_v3(data):
     print(f"Error check(V3): {error_check.hex()}")
     print(f"End bit(V3): {end_bit.hex()}")
     
+def parse_data_v4(data):
+    start_bit = data[0:2]
+    length = data[2]
+    protcol_number = data[3:4]
+    information_serial_number = data[4:6]
+    error_check = data[6:8]
+    end_bit = data[8:10]
+    
+    print(f"Start bit(V4): {start_bit.hex()}")
+    print(f"Length(V4): {length}")
+    print(f"Protocol Number(V4): {protocol_number.hex()}")
+    print(f"Information serial number(V4): {information_serial_number.hex()}")
+    print(f"Error check(V4): {error_check.hex()}")
+    print(f"End bit(V4): {end_bit.hex()}")
+    
     
 # TCP Client
 def tcp_client(hex_data):
